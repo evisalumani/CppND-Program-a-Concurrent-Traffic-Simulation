3568ba350679c3639e8ed083f461831038897373
Evisa Lumani <evisa.lumani@bmw.de>
2020-06-22 11:12:16 +0200

    Task FP.2

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <random>
+#include <chrono>
 #include "TrafficLight.h"
 
 /* Implementation of class "MessageQueue" */
@@ -23,7 +24,7 @@ void MessageQueue<T>::send(T &&msg)
 
 /* Implementation of class "TrafficLight" */
 
-/* 
+
 TrafficLight::TrafficLight()
 {
     _currentPhase = TrafficLightPhase::red;
@@ -44,6 +45,7 @@ TrafficLightPhase TrafficLight::getCurrentPhase()
 void TrafficLight::simulate()
 {
     // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class. 
+    threads.emplace_back(std::thread(&TrafficLight::cycleThroughPhases, this));
 }
 
 // virtual function which is executed in a thread
@@ -52,7 +54,36 @@ void TrafficLight::cycleThroughPhases()
     // FP.2a : Implement the function with an infinite loop that measures the time between two loop cycles 
     // and toggles the current phase of the traffic light between red and green and sends an update method 
     // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds. 
-    // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles. 
-}
+    // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles.
 
-*/
+    // Generate random value (source from Stackoverflow: https://stackoverflow.com/a/7560564)
+    std::mt19937 gen(std::random_device{}()); // generator
+    std::uniform_int_distribution<> distribution(4, 6); // range
+    int cycleDuration = distribution(gen);
+
+    // timestamp at the start of the simulation
+    // the value will be updated inside the loop to indicate the timestamp recorded on the previous loop
+    auto previousLoopTime = std::chrono::high_resolution_clock::now(); 
+    
+    while (true) { 
+        std::this_thread::sleep_for(std::chrono::milliseconds(1)); // wait 1ms between two cycles
+
+        auto currentLoopTime = std::chrono::high_resolution_clock::now(); // timestamp during the simulation
+
+        // Make a cycle run for the given duration, by iteratively checking if the duration has been passed
+        auto currentDuration = std::chrono::duration_cast<std::chrono::seconds>(currentLoopTime - previousLoopTime).count();
+        
+        if (currentDuration >= cycleDuration) {
+            // Start a new cycle wiht toggled traffic light and new random duration
+            if (_currentPhase == TrafficLightPhase::red) 
+                _currentPhase = TrafficLightPhase::green;
+            else 
+                _currentPhase = TrafficLightPhase::red;
+            
+            // TODO: send an update to the message queue
+
+            cycleDuration = distribution(gen);
+            previousLoopTime = std::chrono::high_resolution_clock::now();
+        }
+    }
+}
